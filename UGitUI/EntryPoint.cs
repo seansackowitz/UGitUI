bc492e7c7ed6c49cda1d5b90233051c1b49a24bf
Sean Sackowitz <sean.sackowitz@gmail.com>
2015-11-08 13:55:47 -0500

    Initial

---
+++ UGitUI/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UGitUI
+{
+    class EntryPoint
+    {
+        [STAThread]
+        public static void Main(string[] param)
+        {
+            //Resolves any assembly not found errors. This allows us to compile the dlls into the project.
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
+                string resourceName = "Code_Snapshot.AssemblyLoading." + new AssemblyName(args.Name).Name + ".dll";
+                using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    byte[] assemblyData = new byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
+
+            App.Main();
+        }
+    }
+}
---
+++ UGitUI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UGitUI
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
